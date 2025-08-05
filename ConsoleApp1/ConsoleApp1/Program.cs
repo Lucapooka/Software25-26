2ca8d54eeddcbd76dad3441198b825a620283f44
Lucapooka <Lvfru131@learner.dvusd.org>
2025-08-05 08:11:58 -0700

    student questions

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string name="Bob";
+Console.WriteLine($"Hello " + name);
+Console.WriteLine($"Hello {name}");
