5f93d564c8bec2b8c0d2b9cc8346aaacd3378279
Lucapooka <Lvfru131@learner.dvusd.org>
2025-08-06 07:56:31 -0700

    First program

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("Hello, World!");
 string name="Bob";
 Console.WriteLine($"Hello " + name);
 Console.WriteLine($"Hello {name}");
+Console.WriteLine("what's your name?");
+name = Console.ReadLine();
+Console.WriteLine($"nice to meet you {name}");
+Console.WriteLine($"what's your first name? ");
+Console.WriteLine($"what's your last name? ");
+Console.WriteLine($"what's your student ID? ");
+Console.WriteLine($"Luca ");
+Console.WriteLine($"frusciano ");
+Console.WriteLine($"572131 ");
---
+++ Consoleprogram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// See 
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Recipient's Full Name: ");
+        string recipient = Console.ReadLine();
+
+        Console.Write("Your Full Name (Sender): ");
+        string sender = Console.ReadLine();
+
+        Console.Write("Subject of the Letter: ");
+        string subject = Console.ReadLine();
+
+        Console.Write("Project or Task to Discuss: ");
+        string project = Console.ReadLine();
+
+        Console.Write("Follow-up Date (MM/DD/YYYY): ");
+        string folowDate = Console.ReadLine();
+
+        
+        Console.WriteLine();
+        Console.WriteLine($"Date: {DateTime.Now.ToShortDateString()}");
+        Console.WriteLine($"To: {recipient}");
+       
+        Console.WriteLine($"Dear {recipient},");
+      
+        Console.WriteLine($"Subject: {subject}");
+
+        Console.WriteLine($"Hey, I am writing to discuss the progress of the \"{project}\" project. Your hard work have been very helpful, and I appreciate your dedication to this project.");
+       
+        Console.WriteLine($"As the group moves forward, note that our next follow up is scheduled for {folowDate}. If you have any questions feel free to reach out.");
+       
+        Console.WriteLine("From,");
+        Console.WriteLine(sender);
+    }
+}
